102b88d66f1a89409a3f9baac0a5ee585a015b49
Paulo-Baptista21 <paulohenrique_2112@hotmail.com>
2026-07-31 13:13:57 -0300

    Add nome_comercial class with methods for associations
    Implement class methods for associating active ingredients and presentations.

---
+++ src/vetbridge/domain/nome_comercial.py
@@ -0,0 +1,23 @@
+def __init__(self, id, nome, fabricante):
+        self.id = id
+        self.nome = nome
+        self.fabricante = fabricante
+
+        self.principios_ativos = []
+        self.apresentacoes = []
+
+        fabricante.associar_nome_comercial(self)
+
+    def associar_principio_ativo(self, principio_ativo):
+        if principio_ativo not in self.principios_ativos:
+            self.principios_ativos.append(principio_ativo)
+
+        if self not in principio_ativo.nomes_comerciais:
+            principio_ativo.nomes_comerciais.append(self)
+
+    def adicionar_apresentacao(self, apresentacao):
+        if apresentacao not in self.apresentacoes:
+            self.apresentacoes.append(apresentacao)
+
+        if apresentacao.nome_comercial is not self:
+            apresentacao.nome_comercial = self
