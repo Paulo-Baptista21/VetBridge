578184ba1dd9f74747f510b7eec86f1c8dbce6b1
Paulo-Baptista21 <paulohenrique_2112@hotmail.com>
2026-07-31 13:12:32 -0300

    Fix associating active principle with clinical signal

--- src/vetbridge/domain/sinal_clinico.py
+++ src/vetbridge/domain/sinal_clinico.py
@@ -1,7 +1,6 @@
 class SinalClinico:
 
     def __init__(self, id, nome, descricao):
-
         self.id = id
         self.nome = nome
         self.descricao = descricao
@@ -9,5 +8,8 @@ class SinalClinico:
         self.principios_ativos = []
 
     def associar_principio_ativo(self, principio_ativo):
+        if principio_ativo not in self.principios_ativos:
+            self.principios_ativos.append(principio_ativo)
 
-        self.principios_ativos.append(principio_ativo)
+        if self not in principio_ativo.sinais_clinicos:
+            principio_ativo.sinais_clinicos.append(self)
