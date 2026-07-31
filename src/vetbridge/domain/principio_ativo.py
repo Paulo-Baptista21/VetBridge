1ff4a4ff0a8fd674d05fca1ff68343e2ae7a55f5
Paulo-Baptista21 <paulohenrique_2112@hotmail.com>
2026-07-31 13:12:08 -0300

    Refactor PrincipioAtivo class to manage associations

--- src/vetbridge/domain/principio_ativo.py
+++ src/vetbridge/domain/principio_ativo.py
@@ -1,20 +1,24 @@
 class PrincipioAtivo:
 
     def __init__(self, id, nome, classe_farmacologica, descricao):
-
         self.id = id
         self.nome = nome
         self.classe_farmacologica = classe_farmacologica
         self.descricao = descricao
 
+        self.sinais_clinicos = []
         self.nomes_comerciais = []
 
-        self.sinais_clinicos = []
+    def associar_sinal_clinico(self, sinal_clinico):
+        if sinal_clinico not in self.sinais_clinicos:
+            self.sinais_clinicos.append(sinal_clinico)
+
+        if self not in sinal_clinico.principios_ativos:
+            sinal_clinico.principios_ativos.append(self)
 
     def associar_nome_comercial(self, nome_comercial):
+        if nome_comercial not in self.nomes_comerciais:
+            self.nomes_comerciais.append(nome_comercial)
 
-        self.nomes_comerciais.append(nome_comercial)
-
-    def associar_sinal_clinico(self, sinal_clinico):
-
-        self.sinais_clinicos.append(sinal_clinico)
+        if self not in nome_comercial.principios_ativos:
+            nome_comercial.principios_ativos.append(self)
